24fca0924cdf85be2fe808815dfe5f494c0815c4
Gabrieli <124809091+gabiiloira@users.noreply.github.com>
2023-06-07 17:06:04 -0300

    Add files via upload

---
+++ TabelaAminoacidos/src/Tabela/mainProduto.java
@@ -0,0 +1,30 @@
+package Tabela;
+
+public class mainProduto {
+
+	public static void main(String[] args) {
+
+System.out.println("AUG - Met(Metionina/CÓDON DE INÍCIO)");
+System.out.println("UAA  UAG  UGA  - CÓDON DE PARADA)");
+
+System.out.println("UUU  UUC - Phe(Fenilalamina)");
+System.out.println("UUA  UUG  CUU  CUC  CUA  CUG - Leu(Leucina)");
+System.out.println("AUU  AUC  AUA - Ile(Isoleucina)");
+System.out.println("GUU  GUC  GUA  GUG - Val(Valina)");
+System.out.println("UCU  UCC  UCA  UCG  AGU  AGC - Ser(Serina)");
+System.out.println("CCU  CCC  CCA  CCG - Pro(Prolina)");
+System.out.println("ACU  ACC  ACA  ACG - Thr(Treonina)");
+System.out.println("GCU  GCC  GCA  GCG - Ala(Alanina)");
+System.out.println("UAU  UAC - Tyr(Tirosina)");
+System.out.println("CAU  CAC - His(Histidina)");
+System.out.println("CAA  CAG - Gln(Glutamina)");
+System.out.println("AAU  AAC - Asn(Aspargina)");
+System.out.println("AAA  AAG - Lys(Lisina)");
+System.out.println("GAU  GAC - Asp(Ácido aspártico)");
+System.out.println("GAA  GAG - Glu(Ácido glutâmino)");
+System.out.println("UGU  UGC - Cys(Cisteína)");
+System.out.println("UGG - Trp(Tripcofano)");
+System.out.println("CGU  CGC  CGA  CGG AGA  AGG - Arg(Arginina)");
+System.out.println("GGU  GGC  GGA  GGG - Gly(Glicina)");
+}
+}
